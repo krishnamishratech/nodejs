51b317d23f9e78144b8000c9d6123fa8d2bd9673
andykuang1 <gnauK.ydnA@hotmail.com>
2018-10-13 14:54:16 -0700

    Updated original js to include explanation of parts

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,10 @@
 // Sets computer up as a server that prints Hello World if anyone accesses port 8080
 var http = require('http');
 
+//create server object
 http.createServer(function (req, res) {
+	//status code
 	res.writeHead(200, {'Content-Type': 'text/html'});
+	//end response
 	res.end('Hello World!');
 }).listen(8080);
