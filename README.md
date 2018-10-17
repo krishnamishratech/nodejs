6d5d2bb942aac31023eae3a15e3f6f6dabc95667
andykuang1 <gnauK.ydnA@hotmail.com>
2018-10-17 16:46:59 -0700

    Added url demo and updated other comments

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 Learning node.js through w3schools
 
 
-### myfirst.js | demo_module.js
+### myfirst.js | demo\_module.js | demo\_url.js
 In command line, run
 ```
 node myfirst.js
 ```
 to setup computer as a server, which can then be accessed on the web browser with
 http://localhost:8080
+For url, we can add a '/' and any url to have the server print out the accessed url
--- demo_module.js
+++ demo_module.js
@@ -1,9 +1,11 @@
-// Sets computer up as a server that prints Hello World if anyone accesses port 8080
+// Sets computer up as a server that prints the date if anyone accesses port 8080
 var http = require('http');
 var dt = require('./myfirstmodule');
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('The current date and time is: ' + dt.myDateTime());
-	res.end();
-}).listen(8080);
+http.createServer(
+	function (req, res) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('The current date and time is: ' + dt.myDateTime());
+		res.end();
+	}
+).listen(8080);
